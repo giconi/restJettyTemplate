4708cc7888f5092ba01131d889727ca77b858bdb
matt <matt@localhost>
2021-01-24 13:36:41 +0000

    Added example of parameter collection

--- src/main/java/restTemplateJetty/EntryPoint.java
+++ src/main/java/restTemplateJetty/EntryPoint.java
@@ -1,9 +1,12 @@
 package restTemplateJetty;
 
 import javax.ws.rs.GET;
+
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Random;
 
 
 @Path("/entry-point")
@@ -17,5 +20,19 @@ public class EntryPoint {
 		    public String test() {
 		        return "Test";
 		    }
+		  
+		  @GET
+		  @Path("random")
+		  @Produces(MediaType.TEXT_PLAIN)
+		  	public float random() {
+			  	Random rand = new Random();
+			  	return rand.nextFloat();
+		  }
+		  
+		  @GET
+		  @Path("item/{id}")
+		  public String itemId(@PathParam("id") Integer id) {
+		      return "Path parameter value is [" + id + "]";
+		  }
 
 }
